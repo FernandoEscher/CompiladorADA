221e561c6bb3f83c562a5b51598c02ed4422d16e
FernandoEscher <fernando.irias@escolarea.com>
2009-09-22 20:49:47 -0600

    Parseo completo. Falta recuperación de errores. Agregador prueba.adb

---
+++ prueba.adb
@@ -0,0 +1,16 @@
+
+procedure Holamundo (x: in integer) is
+
+type registro is
+record
+	x : integer;
+	x : float := 5.5;
+end record;
+
+
+begin
+null;
+variable := a*b+"hola"+44;
+end Holamundo;
+
+function F (i: out float) return float
